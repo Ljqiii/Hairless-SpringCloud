d633b884a5ff3f2f2e2bbe4af3d4bdab875c8fa9
Ljqiii <ljq917181927@gmail.com>
2020-03-20 22:44:53 +0800

    test2 not ok

--- Hairless-Auth/src/main/java/com/ljqiii/hairlessauth/config/SecurityConfiguration.java
+++ Hairless-Auth/src/main/java/com/ljqiii/hairlessauth/config/SecurityConfiguration.java
@@ -1,120 +1,120 @@
-//package com.ljqiii.hairlessauth.config;
+package com.ljqiii.hairlessauth.config;
+
+import com.ljqiii.hairlessauth.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+//@EnableWebSecurity
+@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //
-//import com.ljqiii.hairlessauth.dao.UserMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.util.Arrays;
-//import java.util.Collection;
-//
-//
-////@EnableWebSecurity
-//@Configuration
-////@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-////
-////    @Autowired
-////    UserMapper userMapper;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//
-//    @Bean
-//    @Primary
-//    @Override
-//    public UserDetailsService userDetailsServiceBean() throws Exception {
-//        return new UserDetailsService() {
-//            @Override
-//            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//                return new UserDetails() {
-//                    @Override
-//                    public Collection<? extends GrantedAuthority> getAuthorities() {
-//                        return Arrays.asList(new SimpleGrantedAuthority("user"));
-//                    }
-//
-//                    @Override
-//                    public String getPassword() {
-//                        return new BCryptPasswordEncoder().encode("123456");
-//                    }
-//
-//                    @Override
-//                    public String getUsername() {
-//                        return username;
-//                    }
-//
-//                    @Override
-//                    public boolean isAccountNonExpired() {
-//                        return true;
-//                    }
-//
-//                    @Override
-//                    public boolean isAccountNonLocked() {
-//                        return true;
-//                    }
-//
-//                    @Override
-//                    public boolean isCredentialsNonExpired() {
-//                        return true;
-//                    }
-//
-//                    @Override
-//                    public boolean isEnabled() {
-//                        return true;
-//                    }
-//                };
-//            }
-//        };
-//    }
-//
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userDetailsService())
-//                .passwordEncoder(passwordEncoder());
-//
-//    }
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests().anyRequest().authenticated()
-//                .and()
-//                .csrf().disable();
-//    }
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//
-//}
+//    @Autowired
+//    UserMapper userMapper;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    @Primary
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return new UserDetails() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return Arrays.asList(new SimpleGrantedAuthority("user"));
+                    }
+
+                    @Override
+                    public String getPassword() {
+                        return new BCryptPasswordEncoder().encode("123456");
+                    }
+
+                    @Override
+                    public String getUsername() {
+                        return username;
+                    }
+
+                    @Override
+                    public boolean isAccountNonExpired() {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean isAccountNonLocked() {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean isCredentialsNonExpired() {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean isEnabled() {
+                        return true;
+                    }
+                };
+            }
+        };
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+}
