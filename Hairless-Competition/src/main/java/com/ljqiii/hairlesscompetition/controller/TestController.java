82eb35eb675e74c1bcd041f13c4f83c90cfba82e
Ljqiii <ljq917181927@gmail.com>
2020-05-14 01:01:36 +0800

    添加zipkin链路追踪，config server

--- Hairless-Competition/src/main/java/com/ljqiii/hairlesscompetition/controller/TestController.java
+++ Hairless-Competition/src/main/java/com/ljqiii/hairlesscompetition/controller/TestController.java
@@ -2,6 +2,7 @@ package com.ljqiii.hairlesscompetition.controller;
 
 import com.ljqiii.hairlesscompetition.service.CompetitionLeaderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${testljq}")
+    String test;
+
     @Autowired
     CompetitionLeaderService competitionLeaderService;
+
+
+    @GetMapping("/testconfig")
+    public String testconfig() {
+        return test;
+    }
 
     //TODO:delete this after test
     @GetMapping("/calc")
--- Hairless-ConfigServer/src/main/java/com/ljqiii/hairlessconfigserver/HairlessConfigserverApplication.java
+++ Hairless-ConfigServer/src/main/java/com/ljqiii/hairlessconfigserver/HairlessConfigserverApplication.java
@@ -2,8 +2,10 @@ package com.ljqiii.hairlessconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HairlessConfigserverApplication {
 
     public static void main(String[] args) {
